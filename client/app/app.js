0db68326a99e6ab37dd02243c35b96a8a1d03158
Steve Hanna <contact@stevehanna.com>
2016-11-10 15:14:36 -0500

    Default route to login and removed sample Landing and Home pages

--- client/app/app.js
+++ client/app/app.js
@@ -11,7 +11,7 @@ angular.module('angularJsSeedApp', [
 ])
   .config(function ($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider) {
     $urlRouterProvider
-      .otherwise('/');
+      .otherwise('/login');
 
     $locationProvider.html5Mode(true);
     $httpProvider.interceptors.push('authInterceptor');
